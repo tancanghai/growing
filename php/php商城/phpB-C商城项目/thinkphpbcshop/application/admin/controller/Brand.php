5890be2ff8cf31b295243fd6a5d7b60823ad31b2
tancanghai <1131534180@qq.com>
2020-10-19 09:29:32 +0800

    bijiben

--- php/php商城/phpB-C商城项目/thinkphpbcshop/application/admin/controller/Brand.php
+++ php/php商城/phpB-C商城项目/thinkphpbcshop/application/admin/controller/Brand.php
@@ -2,12 +2,14 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Config;
 
 class Brand extends Controller
 {
     public function add()
     {
-        $this->view->engine->layout('common/layout');
+//        $this->view->engine->layout('common/layout');
+//        Config::set('default_ajax_return','html');
         return $this->fetch("add");
     }
 }
--- php/php商城/phpB-C商城项目/thinkphpbcshop/application/admin/controller/Index.php
+++ php/php商城/phpB-C商城项目/thinkphpbcshop/application/admin/controller/Index.php
@@ -2,12 +2,13 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Config;
 
 class Index extends Controller
 {
     public function index()
     {
-        $this->view->engine->layout('common/layout');
+//        $this->view->engine->layout('common/layout');
         return $this->fetch("index");
     }
 }
