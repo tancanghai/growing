7c2624dadd39362739ff1de3dcde753d24ea5514
tancanghai <1131534180@qq.com>
2020-11-19 12:23:34 +0800

    bijiben

---
+++ php/php商城/phpB-C商城项目/thinkphpbcshop/public/static/js/common/cost-common.js
@@ -0,0 +1,8 @@
+var CONSTCONFIG = (function(w){
+    //品牌
+    w.brands = {
+        "BRANDSTATUSNONE":{"VAL":'0',"DESC":"没有"},
+        "BRANDSTATUSHAVE":{"VAL":'1',"DESC":"有"},
+    }
+    return w;
+})(window);
